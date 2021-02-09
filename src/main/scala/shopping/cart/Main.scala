04357c37e55fac377045371f0dbccf6d0b3139a7
Renato Cavalcanti <renato@cavalcanti.be>
2021-02-09 21:41:29 +0100

    initial commit

---
+++ src/main/scala/shopping/cart/Main.scala
@@ -0,0 +1,52 @@
+package shopping.cart
+
+import akka.actor.typed.scaladsl.AbstractBehavior
+import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.Behavior
+import akka.management.cluster.bootstrap.ClusterBootstrap
+import akka.management.scaladsl.AkkaManagement
+import shopping.cart.repository.ItemPopularityRepositoryImpl
+import shopping.cart.repository.ScalikeJdbcSetup
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    ActorSystem[Nothing](Main(), "ShoppingCartService")
+  }
+
+  def apply(): Behavior[Nothing] = {
+    Behaviors.setup[Nothing](context => new Main(context))
+  }
+}
+
+class Main(context: ActorContext[Nothing])
+    extends AbstractBehavior[Nothing](context) {
+  val system = context.system
+
+  
+  ScalikeJdbcSetup.init(system) 
+  
+
+  AkkaManagement(system).start()
+  ClusterBootstrap(system).start()
+
+  ShoppingCart.init(system)
+
+  
+  val itemPopularityRepository = new ItemPopularityRepositoryImpl() 
+  ItemPopularityProjection.init(system, itemPopularityRepository) 
+  
+
+  val grpcInterface =
+    system.settings.config.getString("shopping-cart-service.grpc.interface")
+  val grpcPort =
+    system.settings.config.getInt("shopping-cart-service.grpc.port")
+  val grpcService =
+    new ShoppingCartServiceImpl(system, itemPopularityRepository)
+  ShoppingCartServer.start(grpcInterface, grpcPort, system, grpcService)
+
+  override def onMessage(msg: Nothing): Behavior[Nothing] =
+    this
+}
