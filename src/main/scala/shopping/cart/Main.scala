2dca88952fe71503570f8b366ce2f1af2191f10c
Renato Cavalcanti <renato@cavalcanti.be>
2021-02-10 15:45:23 +0100

    enable grpc service

--- src/main/scala/shopping/cart/Main.scala
+++ src/main/scala/shopping/cart/Main.scala
@@ -1,7 +1,28 @@
 package shopping.cart
 
+import scala.concurrent.Future
+
+import akka.actor.typed.ActorSystem
 import akka.application.internal.AkkaClusterApplication
+import akka.grpc.scaladsl.ServerReflection
+import akka.grpc.scaladsl.ServiceHandler
+import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.model.HttpResponse
 
 object Main extends AkkaClusterApplication {
+
   override val applicationName: String = "ShoppingCartService"
+
+  /**
+   * User main function.
+   */
+  override def init(args: Array[String])(implicit system: ActorSystem[Nothing]): HttpRequest => Future[HttpResponse] = {
+
+    // user gRPC service
+    val service = new ShoppingCartServiceImpl(system)
+    ServiceHandler.concatOrNotFound(
+      proto.ShoppingCartServiceHandler.partial(service),
+      // ServerReflection enabled to support grpcurl without import-path and proto parameters
+      ServerReflection.partial(List(proto.ShoppingCartService)))
+  }
 }
--- src/main/scala/shopping/cart/ShoppingCartServiceImpl.scala
+++ src/main/scala/shopping/cart/ShoppingCartServiceImpl.scala
@@ -1,20 +1,16 @@
 package shopping.cart
 
 import java.util.concurrent.TimeoutException
-import scala.concurrent.{ ExecutionContext, Future }
-import akka.actor.typed.{ ActorSystem, DispatcherSelector }
-import akka.cluster.sharding.typed.scaladsl.ClusterSharding
+
+import scala.concurrent.Future
+
+import akka.actor.typed.ActorSystem
 import akka.grpc.GrpcServiceException
 import akka.util.Timeout
 import io.grpc.Status
 import org.slf4j.LoggerFactory
-import shopping.cart.repository.{ ItemPopularityRepository, ScalikeJdbcSession }
 
-import akka.actor.typed.ActorRef
-import akka.pattern.StatusReply
-
-class ShoppingCartServiceImpl(system: ActorSystem[_], itemPopularityRepository: ItemPopularityRepository)
-    extends proto.ShoppingCartService {
+class ShoppingCartServiceImpl(system: ActorSystem[_]) extends proto.ShoppingCartService {
 
   import system.executionContext
 
@@ -23,57 +19,21 @@ class ShoppingCartServiceImpl(system: ActorSystem[_], itemPopularityRepository: 
   implicit private val timeout: Timeout =
     Timeout.create(system.settings.config.getDuration("shopping-cart-service.ask-timeout"))
 
-  private val sharding = ClusterSharding(system)
+  //  private val sharding = ClusterSharding(system)
 
-  private val blockingJdbcExecutor: ExecutionContext =
-    system.dispatchers.lookup(DispatcherSelector.fromConfig("akka.projection.jdbc.blocking-jdbc-dispatcher"))
+  //  private val blockingJdbcExecutor: ExecutionContext =
+  //    system.dispatchers.lookup(DispatcherSelector.fromConfig("akka.projection.jdbc.blocking-jdbc-dispatcher"))
 
   override def addItem(in: proto.AddItemRequest): Future[proto.Cart] = {
     logger.info("addItem {} to cart {}", in.itemId, in.cartId)
-    val entityRef = sharding.entityRefFor(ShoppingCart.EntityKey, in.cartId)
-    val reply: Future[ShoppingCart.Summary] =
-      entityRef.askWithStatus(ShoppingCart.AddItem(in.itemId, in.quantity, _))
-    val response = reply.map(cart => toProtoCart(cart))
-    convertError(response)
+    Future.successful(proto.Cart(items = List(proto.Item(in.itemId, in.quantity))))
   }
 
-  override def updateItem(in: proto.UpdateItemRequest): Future[proto.Cart] = {
-    logger.info("updateItem {} to cart {}", in.itemId, in.cartId)
-    val entityRef = sharding.entityRefFor(ShoppingCart.EntityKey, in.cartId)
+  override def updateItem(in: proto.UpdateItemRequest): Future[proto.Cart] = ???
 
-    def command(replyTo: ActorRef[StatusReply[ShoppingCart.Summary]]) =
-      if (in.quantity == 0)
-        ShoppingCart.RemoveItem(in.itemId, replyTo)
-      else
-        ShoppingCart.AdjustItemQuantity(in.itemId, in.quantity, replyTo)
+  override def checkout(in: proto.CheckoutRequest): Future[proto.Cart] = ???
 
-    val reply: Future[ShoppingCart.Summary] =
-      entityRef.askWithStatus(command(_))
-    val response = reply.map(cart => toProtoCart(cart))
-    convertError(response)
-  }
-
-  override def checkout(in: proto.CheckoutRequest): Future[proto.Cart] = {
-    logger.info("checkout {}", in.cartId)
-    val entityRef = sharding.entityRefFor(ShoppingCart.EntityKey, in.cartId)
-    val reply: Future[ShoppingCart.Summary] =
-      entityRef.askWithStatus(ShoppingCart.Checkout(_))
-    val response = reply.map(cart => toProtoCart(cart))
-    convertError(response)
-  }
-
-  override def getCart(in: proto.GetCartRequest): Future[proto.Cart] = {
-    logger.info("getCart {}", in.cartId)
-    val entityRef = sharding.entityRefFor(ShoppingCart.EntityKey, in.cartId)
-    val response =
-      entityRef.ask(ShoppingCart.Get).map { cart =>
-        if (cart.items.isEmpty)
-          throw new GrpcServiceException(Status.NOT_FOUND.withDescription(s"Cart ${in.cartId} not found"))
-        else
-          toProtoCart(cart)
-      }
-    convertError(response)
-  }
+  override def getCart(in: proto.GetCartRequest): Future[proto.Cart] = ???
 
   private def toProtoCart(cart: ShoppingCart.Summary): proto.Cart = {
     proto.Cart(
@@ -92,16 +52,5 @@ class ShoppingCartServiceImpl(system: ActorSystem[_], itemPopularityRepository: 
     }
   }
 
-  override def getItemPopularity(in: proto.GetItemPopularityRequest): Future[proto.GetItemPopularityResponse] = {
-    Future {
-      ScalikeJdbcSession.withSession { session =>
-        itemPopularityRepository.getItem(session, in.itemId)
-      }
-    }(blockingJdbcExecutor).map {
-      case Some(count) =>
-        proto.GetItemPopularityResponse(in.itemId, count)
-      case None =>
-        proto.GetItemPopularityResponse(in.itemId, 0L)
-    }
-  }
+  override def getItemPopularity(in: proto.GetItemPopularityRequest): Future[proto.GetItemPopularityResponse] = ???
 }
