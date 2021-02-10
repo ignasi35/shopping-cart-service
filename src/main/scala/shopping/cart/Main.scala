df55058af66e875d3be71badb8e986b90dbac2ae
Renato Cavalcanti <renato@cavalcanti.be>
2021-02-10 16:00:38 +0100

    entity and projection

--- src/main/scala/shopping/cart/Main.scala
+++ src/main/scala/shopping/cart/Main.scala
@@ -8,6 +8,8 @@ import akka.grpc.scaladsl.ServerReflection
 import akka.grpc.scaladsl.ServiceHandler
 import akka.http.scaladsl.model.HttpRequest
 import akka.http.scaladsl.model.HttpResponse
+import shopping.cart.repository.ItemPopularityRepositoryImpl
+import shopping.cart.repository.ScalikeJdbcSetup
 
 object Main extends AkkaClusterApplication {
 
@@ -18,8 +20,20 @@ object Main extends AkkaClusterApplication {
    */
   override def init(args: Array[String])(implicit system: ActorSystem[Nothing]): HttpRequest => Future[HttpResponse] = {
 
+    // user DB tech choice bootstrap
+    ScalikeJdbcSetup.init(system)
+
+    // user entity
+    ShoppingCart.init(system)
+
+    // user read-side repository
+    val repository = new ItemPopularityRepositoryImpl
+
+    // user read-side projection
+    ItemPopularityProjection.init(system, repository)
+
     // user gRPC service
-    val service = new ShoppingCartServiceImpl(system)
+    val service = new ShoppingCartServiceImpl(system, repository)
     ServiceHandler.concatOrNotFound(
       proto.ShoppingCartServiceHandler.partial(service),
       // ServerReflection enabled to support grpcurl without import-path and proto parameters
--- src/main/scala/shopping/cart/ShoppingCartServiceImpl.scala
+++ src/main/scala/shopping/cart/ShoppingCartServiceImpl.scala
@@ -2,15 +2,23 @@ package shopping.cart
 
 import java.util.concurrent.TimeoutException
 
+import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
+import akka.actor.typed.ActorRef
 import akka.actor.typed.ActorSystem
+import akka.actor.typed.DispatcherSelector
+import akka.cluster.sharding.typed.scaladsl.ClusterSharding
 import akka.grpc.GrpcServiceException
+import akka.pattern.StatusReply
 import akka.util.Timeout
 import io.grpc.Status
 import org.slf4j.LoggerFactory
+import shopping.cart.repository.ItemPopularityRepository
+import shopping.cart.repository.ScalikeJdbcSession
 
-class ShoppingCartServiceImpl(system: ActorSystem[_]) extends proto.ShoppingCartService {
+class ShoppingCartServiceImpl(system: ActorSystem[_], itemPopularityRepository: ItemPopularityRepository)
+    extends proto.ShoppingCartService {
 
   import system.executionContext
 
@@ -19,21 +27,57 @@ class ShoppingCartServiceImpl(system: ActorSystem[_]) extends proto.ShoppingCart
   implicit private val timeout: Timeout =
     Timeout.create(system.settings.config.getDuration("shopping-cart-service.ask-timeout"))
 
-  //  private val sharding = ClusterSharding(system)
+  private val sharding = ClusterSharding(system)
 
-  //  private val blockingJdbcExecutor: ExecutionContext =
-  //    system.dispatchers.lookup(DispatcherSelector.fromConfig("akka.projection.jdbc.blocking-jdbc-dispatcher"))
+  private val blockingJdbcExecutor: ExecutionContext =
+    system.dispatchers.lookup(DispatcherSelector.fromConfig("akka.projection.jdbc.blocking-jdbc-dispatcher"))
 
   override def addItem(in: proto.AddItemRequest): Future[proto.Cart] = {
     logger.info("addItem {} to cart {}", in.itemId, in.cartId)
-    Future.successful(proto.Cart(items = List(proto.Item(in.itemId, in.quantity))))
+    val entityRef = sharding.entityRefFor(ShoppingCart.EntityKey, in.cartId)
+    val reply: Future[ShoppingCart.Summary] =
+      entityRef.askWithStatus(ShoppingCart.AddItem(in.itemId, in.quantity, _))
+    val response = reply.map(cart => toProtoCart(cart))
+    convertError(response)
   }
 
-  override def updateItem(in: proto.UpdateItemRequest): Future[proto.Cart] = ???
+  override def updateItem(in: proto.UpdateItemRequest): Future[proto.Cart] = {
+    logger.info("updateItem {} to cart {}", in.itemId, in.cartId)
+    val entityRef = sharding.entityRefFor(ShoppingCart.EntityKey, in.cartId)
 
-  override def checkout(in: proto.CheckoutRequest): Future[proto.Cart] = ???
+    def command(replyTo: ActorRef[StatusReply[ShoppingCart.Summary]]) =
+      if (in.quantity == 0)
+        ShoppingCart.RemoveItem(in.itemId, replyTo)
+      else
+        ShoppingCart.AdjustItemQuantity(in.itemId, in.quantity, replyTo)
 
-  override def getCart(in: proto.GetCartRequest): Future[proto.Cart] = ???
+    val reply: Future[ShoppingCart.Summary] =
+      entityRef.askWithStatus(command(_))
+    val response = reply.map(cart => toProtoCart(cart))
+    convertError(response)
+  }
+
+  override def checkout(in: proto.CheckoutRequest): Future[proto.Cart] = {
+    logger.info("checkout {}", in.cartId)
+    val entityRef = sharding.entityRefFor(ShoppingCart.EntityKey, in.cartId)
+    val reply: Future[ShoppingCart.Summary] =
+      entityRef.askWithStatus(ShoppingCart.Checkout(_))
+    val response = reply.map(cart => toProtoCart(cart))
+    convertError(response)
+  }
+
+  override def getCart(in: proto.GetCartRequest): Future[proto.Cart] = {
+    logger.info("getCart {}", in.cartId)
+    val entityRef = sharding.entityRefFor(ShoppingCart.EntityKey, in.cartId)
+    val response =
+      entityRef.ask(ShoppingCart.Get).map { cart =>
+        if (cart.items.isEmpty)
+          throw new GrpcServiceException(Status.NOT_FOUND.withDescription(s"Cart ${in.cartId} not found"))
+        else
+          toProtoCart(cart)
+      }
+    convertError(response)
+  }
 
   private def toProtoCart(cart: ShoppingCart.Summary): proto.Cart = {
     proto.Cart(
@@ -52,5 +96,16 @@ class ShoppingCartServiceImpl(system: ActorSystem[_]) extends proto.ShoppingCart
     }
   }
 
-  override def getItemPopularity(in: proto.GetItemPopularityRequest): Future[proto.GetItemPopularityResponse] = ???
+  override def getItemPopularity(in: proto.GetItemPopularityRequest): Future[proto.GetItemPopularityResponse] = {
+    Future {
+      ScalikeJdbcSession.withSession { session =>
+        itemPopularityRepository.getItem(session, in.itemId)
+      }
+    }(blockingJdbcExecutor).map {
+      case Some(count) =>
+        proto.GetItemPopularityResponse(in.itemId, count)
+      case None =>
+        proto.GetItemPopularityResponse(in.itemId, 0L)
+    }
+  }
 }
